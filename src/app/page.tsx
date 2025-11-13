c193ee15d54b4758b4e60e7e843cb0b775e267ed
Nguyen Trong Nhan <nhannt@tts.tvtlive.vn>
2025-11-13 17:32:16 +0700

    landingpage on+

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,126 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="container ">
+			<div className="main flex flex-col justify-start items-start">
+				<div className="title flex">
+					<img
+						src="/images/41205a56f52c4a049eb751611a2967f6a75f8326.png"
+						alt=""
+						className="logo"
+					/>
+					<div className="desc flex flex-col">
+						<div className="desc1">SIÊU ỨNG DỤNG THỂ THAO, GIẢI TRÍ</div>
+						<div className="desc2">HÀNG ĐẦU VIỆT NAM</div>
+					</div>
+				</div>
+
+				<ul className="list flex flex-col">
+					<li className="item flex">
+						<img src="/images/tick-circle.png" alt="" />
+						<div className="desc">
+							<span className="desc1">
+								Xem trực tiếp các giải thể thao đẳng cấp:
+								<br />
+							</span>
+							Champions League, Europa League, Ligue 1, Serie A, Tennis, Bóng
+							chuyền,...
+						</div>
+					</li>
+					<li className="item flex">
+						<img src="/images/tick-circle.png" alt="" />
+						<div className="desc">
+							Trọn vẹn đam mê cùng các{' '}
+							<span className="desc2">
+								giải đấu Esports đỉnh cao và giải trí bất tận
+							</span>
+						</div>
+					</li>
+					<li className="item flex">
+						<img src="/images/tick-circle.png" alt="" />
+						<div className="desc">
+							Sở hữu các kênh{' '}
+							<span className="desc2">
+								truyền hình Quốc gia và kênh phát sóng Thể thao đặc sắc
+							</span>
+						</div>
+					</li>
+				</ul>
+
+				<div className="download flex flex-col">
+					<div className="text">ĐĂNG KÝ NGAY</div>
+					<div className="main flex">
+						<img
+							src="/images/b99952171d4b5aa6b0ef84fe88fd7853a5cc12ed.jpg"
+							alt=""
+							className="qr"
+						/>
+						<div className="list flex flex-col">
+							<button className="item flex">
+								<img src="/images/Group 3.png" alt="" />
+							</button>
+							<button className="item flex">
+								<img src="/images/Group 2.png" alt="" />
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="register flex flex-col">
+				<div className="header flex flex-col justify-center items-center">
+					<div className="desc">XEM TRỌN VẸN CÁC GIẢI ĐẤU</div>
+					<div className="price">CHỈ TỪ 7000đ</div>
+				</div>
+				<div className="plans flex flex-row">
+					<div className="plan">
+						<div className="name">ONMAX7</div>
+
+						<div className="sendto">
+							<span>gửi</span>888
+						</div>
+					</div>
+					/
+					<div className="plan">
+						<div className="name">ONMAX10</div>
+
+						<div className="sendto">
+							<span>gửi</span>888
+						</div>
+					</div>
+				</div>
+				<ul className="detail">
+					<li className="text">
+						Data siêu khủng <span>mỗi ngày</span>
+					</li>
+					<li className="text">
+						<span>Nhận 05 lượt quay may mắn với cơ hội trúng </span> <br />
+						IPHONE 17 PRO MAX
+					</li>
+				</ul>
+			</div>
+			<img
+				className="float3"
+				src="/images/50bed5ab36a239ee6003dfcfc727cde38790f12d.png" //laptop
+				alt=""
+			/>
+			<img
+				className="float1"
+				src="/images/562d385efd4b2eafdaead894df984ef74804a617.png" // c1
+				alt=""
+			/>
+			<img
+				className="float2"
+				src="/images/d6b8265343c03dfbcdddb84a4d134d69115add77.png" // mobile tablet
+				alt=""
+			/>
+			<img
+				className="float4"
+				src="/images/Mask group (1).png" // wheel
+				alt=""
+			/>
+			<div className="topleft"></div>
+			<div className="topright"></div>
+		</div>
+	);
 }
